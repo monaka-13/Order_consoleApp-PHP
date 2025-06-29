fed5ae16cf51a024440599857a7e35a64cb12ce1
shigenaga.k <kshigenaga5@gmail.com>
2025-06-29 11:40:39 -0700

    add: constants in config

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -1,2 +1,18 @@
 <?php
-?>
+define("NUMBER_OF_ORDER", 4);
+define("DEVELOPER", "Kay Shigenaga");
+define("MIN_AMOUNT", 0);
+define("MAX_AMOUNT", 10);
+define("TAX_PERCENTAGE", 0.12);
+define("PRICES", [
+  "A" => 30,
+  "B" => 30,
+  "C" => 40
+]);
+define("DISCOUNT_RATES", [
+  0 => 0,
+  200 => 0.05,
+  300 => 0.1,
+  500 => 0.15,
+  700 => 0.25
+]);
