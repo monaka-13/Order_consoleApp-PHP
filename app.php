48a95ea650c8f4a8057753d5341b3978bb05dec0
shigenaga.k <kshigenaga5@gmail.com>
2025-06-29 15:38:02 -0700

    add: getHeader, generateOrder

--- app.php
+++ app.php
@@ -1,4 +1,8 @@
 <?php
 require_once("inc/config.inc.php");
 require_once("inc/functions.inc.php");
+
+getHeader();
+
+print_r(generateOrder());
 ?>
--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -1,2 +1,23 @@
 <?php
+
+function getHeader()
+{
+  echo "----------\nProduct order calculator app by " . DEVELOPER . "\n----------\n";
+}
+function generateOrder()
+{
+  $orderData = [];
+  for ($i = 0; $i < NUMBER_OF_ORDER; $i++) {
+    $amount = [];
+    for ($j = 0; $j < count(PRICES); $j++) {
+      $amount[] = rand(MIN_AMOUNT, MAX_AMOUNT);
+    }
+    $order = [
+      'id' => "order_" . $i + 1,
+      'amount' => $amount
+    ];
+    array_push($orderData, $order);
+  }
+  return $orderData;
+}
 ?>
