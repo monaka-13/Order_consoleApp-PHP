730d3527f1be8c1355961eba6e98286e2241e63e
shigenaga.k <kshigenaga5@gmail.com>
2025-06-30 19:16:01 -0700

    add: 大体実装完了

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -12,31 +12,55 @@ function generateOrder()
     for ($j = 0; $j < count(PRICES); $j++) {
       array_push($amount, rand(MIN_AMOUNT, MAX_AMOUNT));
     }
+    $sum = getSums($amount);
+    $discount = getDiscountedPercentage($sum);
+    $total = $sum * $discount;
+
     $order = [
       "id" => "order_" . $i + 1,
       "amount" => $amount,
-      "discount" => 15, // TODO
-      "subTotal" => 495.04 // TODO
+      "discount" => $discount,
+      "subTotal" => $total
     ];
     array_push($orderData, $order);
   }
   return $orderData;
 }
 
+function getSums($amount)
+{
+  $sum = 0;
+  for ($i = 0; $i < count($amount); $i++) {
+    $sum += $amount[$i] * array_values(PRICES)[$i];
+  }
+  return $sum;
+}
+function getDiscountedPercentage($sum)
+{
+  $rate = 0;
+  foreach (DISCOUNT_RATES as $key => $val) {
+    if ($sum < $key) {
+      return $rate;
+    } else {
+      $rate = $val;
+    }
+  }
+  return $rate;
+}
 function printOrder($orderData)
 {
-$count=1;
+  $count = 1;
   do {
     echo "\nOrder record: " . $count . "\n";
     echo "Order\tProduct A\tProduct B\tProduct C\tDiscount\tSub Total\n";
 
-    foreach ($orderData as $key=>$order) {
+    foreach ($orderData as $key => $order) {
       echo $order["id"] . "\t";
       foreach ($order["amount"] as $key => $val) {
         echo $val . "\t\t";
       }
-      echo $order["discount"] . "\t\t";
-      echo $order["subTotal"] . "\n";
+      echo $order["discount"] * 100 . "%\t\t";
+      echo "$" . number_format($order["subTotal"], 2) . "\n";
     }
     echo "Do you want to add another record of order(Y/N)";
     $input = stream_get_line(STDIN, 1024, PHP_EOL);
